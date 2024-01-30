e809da0262d022f0d0d9b3cf4962d9e13456de38
Crowdbotics <team@crowdbotics.com>
2024-01-30 19:55:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-band-45709",
+  url: "https://little-band-45709.botics.co",
+  api: "https://little-band-45709.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
